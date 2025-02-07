fa0242f309db20c6a3be73ba25199884efca0016
Alexandre Halas <alexandrehalas@gmail.com>
2025-02-07 20:55:56 -0300

    feat: Initial commit

---
+++ src/test/java/br/com/halas/spring_boot_lambda_and_stream/SpringBootLambdaAndStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.halas.spring_boot_lambda_and_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLambdaAndStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
