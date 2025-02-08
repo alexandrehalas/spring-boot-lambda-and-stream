6aed98afae0f32d552125cc9c60ff275a78d51cc
Alexandre Halas <alexandrehalas@gmail.com>
2025-02-08 16:33:44 -0300

    feat: Created a main class to store methods that will accept user input

--- src/main/java/br/com/halas/lambda_and_stream/Application.java
+++ src/main/java/br/com/halas/lambda_and_stream/Application.java
@@ -1,15 +1,9 @@
 package br.com.halas.lambda_and_stream;
 
-import br.com.halas.lambda_and_stream.model.Episode;
-import br.com.halas.lambda_and_stream.model.Season;
-import br.com.halas.lambda_and_stream.model.Serie;
-import br.com.halas.lambda_and_stream.service.ApiConsumptionService;
-import br.com.halas.lambda_and_stream.service.DataConverterService;
+import br.com.halas.lambda_and_stream.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -20,20 +14,7 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        var apiConsumptionService = new ApiConsumptionService();
-        var dataConverterService = new DataConverterService();
-
-        var serieUrl = "https://www.omdbapi.com/?t=supernatural&apikey=d7239a2d";
-        var serieJson = apiConsumptionService.getData(serieUrl);
-        var serie = dataConverterService.getData(serieJson, Serie.class);
-
-        var seasons = new ArrayList<Season>();
-        for (int i = 1; i < serie.totalSeasons(); i++) {
-            var seasonJson = apiConsumptionService.getData("https://www.omdbapi.com/?t=supernatural&season=" + i + "&apikey=d7239a2d");
-            var season = dataConverterService.getData(seasonJson, Season.class);
-            seasons.add(season);
-        }
-        seasons.forEach(System.out::println);
+        Main main = new Main();
+        main.showMenu();
     }
 }
---
+++ src/main/java/br/com/halas/lambda_and_stream/main/Main.java
@@ -0,0 +1,41 @@
+package br.com.halas.lambda_and_stream.main;
+
+import br.com.halas.lambda_and_stream.model.Season;
+import br.com.halas.lambda_and_stream.model.Serie;
+import br.com.halas.lambda_and_stream.service.ApiConsumptionService;
+import br.com.halas.lambda_and_stream.service.DataConverterService;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static final String URL = "https://www.omdbapi.com/?t=";
+    public static final String API_KEY = "&apikey=d7239a2d";
+    public static final String SEASON = "&season=";
+
+    private ApiConsumptionService apiConsumptionService = new ApiConsumptionService();
+    private DataConverterService dataConverterService = new DataConverterService();
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void showMenu() {
+
+        System.out.println("Enter the 'NAME' of the serie");
+        var serieName = scanner.nextLine();
+
+        var serieUrl = URL + serieName.replace(" ", "+") + API_KEY;
+        var serieJson = apiConsumptionService.getData(serieUrl);
+        var serie = dataConverterService.getData(serieJson, Serie.class);
+
+        var seasons = new ArrayList<Season>();
+        for (int i = 1; i < serie.totalSeasons(); i++) {
+            var seasonUrl = URL + serieName.replace(" ", "+") + SEASON + i + API_KEY;
+            var seasonJson = apiConsumptionService.getData(seasonUrl);
+            var season = dataConverterService.getData(seasonJson, Season.class);
+            seasons.add(season);
+        }
+        seasons.forEach(System.out::println);
+    }
+
+}
