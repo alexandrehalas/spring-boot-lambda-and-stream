30a3e1667f63612f750c6a91353cc7af53c497a9
Alexandre Halas <alexandrehalas@gmail.com>
2025-02-08 15:48:36 -0300

    feat: Included Episode model and retrieve this from api

--- src/main/java/br/com/halas/lambda_and_stream/Application.java
+++ src/main/java/br/com/halas/lambda_and_stream/Application.java
@@ -1,5 +1,6 @@
 package br.com.halas.lambda_and_stream;
 
+import br.com.halas.lambda_and_stream.model.Episode;
 import br.com.halas.lambda_and_stream.model.Serie;
 import br.com.halas.lambda_and_stream.service.ApiConsumptionService;
 import br.com.halas.lambda_and_stream.service.DataConverterService;
@@ -23,8 +24,13 @@ public class Application implements CommandLineRunner {
         var serieUrl = "https://www.omdbapi.com/?t=supernatural&apikey=d7239a2d";
         var serieJson = apiConsumptionService.getData(serieUrl);
 
+        var episodeUrl = "https://www.omdbapi.com/?t=supernatural&season=1&episode=1&apikey=d7239a2d";
+        var episodeJson = apiConsumptionService.getData(episodeUrl);
+
         var serie = dataConverterService.getData(serieJson, Serie.class);
+        var episode = dataConverterService.getData(episodeJson, Episode.class);
 
         System.out.println(serie);
+        System.out.println(episode);
     }
 }
---
+++ src/main/java/br/com/halas/lambda_and_stream/model/Episode.java
@@ -0,0 +1,11 @@
+package br.com.halas.lambda_and_stream.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Episode(@JsonAlias("Title") String title,
+                      Integer episode,
+                      @JsonAlias("imdbRating") String rating,
+                      @JsonAlias("Released") String releasedDate) {
+}
